089b9bd940a12e61f1b335f90ecacfbaff70af02
Michael Cassens <michaelcassens@gmail.com>
2020-10-28 10:06:40 -0600

    simple vr example from p5

---
+++ p5xr/sketch.js
@@ -0,0 +1,14 @@
+function preload() {
+    createVRCanvas();
+}
+
+function setup()
+{
+
+}
+
+function calculate() {
+    // code that you want to run once each frame.
+    setVRBackgroundColor(200, 0, 150);
+    circle(100,100,25);
+}
