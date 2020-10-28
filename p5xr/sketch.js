13e32c51c606bcf792855fcc7617819d5f202c98
Michael Cassens <michaelcassens@gmail.com>
2020-10-28 10:25:18 -0600

    Update sketch.js

--- p5xr/sketch.js
+++ p5xr/sketch.js
@@ -9,6 +9,10 @@ function setup()
 
 function calculate() {
     // code that you want to run once each frame.
-    setVRBackgroundColor(200, 0, 150);
-    circle(100,100,2);
+    setVRBackgroundColor(200, 100, 150);
+    circle(0,0,2);
+}
+
+function draw()
+{
 }
